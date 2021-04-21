afd18ebdab43498d43a9962642459e77c65d7f0d
PratyushRoy <77485233+PratyushRoy@users.noreply.github.com>
2021-04-21 20:17:37 +0530

    Add files via upload

---
+++ BlueBox.js
@@ -0,0 +1,23 @@
+class BlueBox extends Box{
+    constructor(x,y){
+        super(x,y,20,30);
+        this.visibility=255;
+        this.image=loadImage("block.png");
+    }
+
+    display(){
+        fill(85,136,238);
+       if(this.body.speed<3){
+       super.display(); 
+        }
+        else{
+            World.remove(world,this.body);
+            push();
+            this.visibility=this.visibility-5;
+            tint(255,this.visibility);
+            image(this.image,this.body.position.x,this.body.position.y,20,30);
+            pop();
+          }
+        
+    }
+}
